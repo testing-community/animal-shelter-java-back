5531feee7ba0aaffe070f139af9803273a6a95fe
Alejandro Perdomo <alejandro.perdomob@perficient.com>
2021-04-07 19:20:50 -0500

    refator(test): remove RestAssured from AnimalDetailTest

--- src/test/java/com/shelter/animalback/component/api/AnimalDetailTest.java
+++ src/test/java/com/shelter/animalback/component/api/AnimalDetailTest.java
@@ -1,26 +1,23 @@
 package com.shelter.animalback.component.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shelter.animalback.controller.dto.AnimalDto;
 import com.shelter.animalback.domain.Animal;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -28,10 +25,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
-public class animalDetailTest {
-
-    @LocalServerPort
-    private int port;
+public class AnimalDetailTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -39,9 +33,8 @@ public class animalDetailTest {
     private AnimalDto cat;
 
     @BeforeEach
-    public void setUp() throws Exception {
-        RestAssured.port = port;
-
+    @SneakyThrows
+    public void setUp() {
         cat = new AnimalDto("Thor", "Birmano", "Male", false, new String[]{"Leucemia Felina"});
         var catString = new ObjectMapper().writeValueAsString(cat);
 
@@ -51,21 +44,23 @@ public class animalDetailTest {
     }
 
     @Test
-    public void animaDetailWithSuccessStatusCodeAndContentType() throws Exception {
+    @SneakyThrows
+    public void animaDetailWithSuccessStatusCodeAndContentType() {
         var response = mockMvc.perform(get("/animals/Thor")).andReturn().getResponse();
 
-        MatcherAssert.assertThat(response.getStatus(), Matchers.equalTo(HttpStatus.OK.value()));
-        MatcherAssert.assertThat(response.getContentType(), Matchers.equalTo(MediaType.APPLICATION_JSON.toString()));
+        assertThat(response.getStatus(), equalTo(HttpStatus.OK.value()));
+        assertThat(response.getContentType(), equalTo(MediaType.APPLICATION_JSON.toString()));
     }
 
     @Test
-    public void detailWithTheRightAnimal() throws Exception {
+    @SneakyThrows
+    public void detailWithTheRightAnimal() {
         var response = mockMvc.perform(get("/animals/Thor")).andReturn().getResponse();
         var animal = new ObjectMapper().readValue(response.getContentAsString(), Animal.class);
 
-        MatcherAssert.assertThat(animal.getName(), Matchers.equalTo(cat.getName()));
-        MatcherAssert.assertThat(animal.getBreed(), Matchers.equalTo(cat.getBreed()));
-        MatcherAssert.assertThat(animal.getGender(), Matchers.equalTo(cat.getGender()));
-        MatcherAssert.assertThat(animal.isVaccinated(), Matchers.equalTo(cat.isVaccinated()));
+        assertThat(animal.getName(), equalTo(cat.getName()));
+        assertThat(animal.getBreed(), equalTo(cat.getBreed()));
+        assertThat(animal.getGender(), equalTo(cat.getGender()));
+        assertThat(animal.isVaccinated(), equalTo(cat.isVaccinated()));
     }
 }
