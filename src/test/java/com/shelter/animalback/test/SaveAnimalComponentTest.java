6369230e74e6e29f7854598898cb98c8f5fc8b8b
Sebastian Villegas Zapata <svillegasz@psl.com.co>
2021-05-10 19:21:52 -0500

    First component test: part 1, migration from api test to component test

---
+++ src/test/java/com/shelter/animalback/test/SaveAnimalComponentTest.java
@@ -0,0 +1,46 @@
+package com.shelter.animalback.test;
+
+import com.shelter.animalback.controller.dto.AnimalDto;
+import com.shelter.animalback.controller.dto.CreateAnimalBodyDto;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SaveAnimalComponentTest {
+    @Test
+    public void testSaveAnimalSuccessfully() {
+        // Arrange - Instance animal with data for request body
+        CreateAnimalBodyDto animalRequest = new CreateAnimalBodyDto();
+        animalRequest.setName("Hela");
+        animalRequest.setBreed("Mestizo");
+        animalRequest.setGender("Female");
+        animalRequest.setVaccinated(false);
+
+        // Act - Make a POST request against the create animal endpoint
+        Response response = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(animalRequest)
+                .when()
+                .post("http://localhost:8080/animals")
+                .thenReturn();
+
+        AnimalDto animalResponse = response.as(AnimalDto.class);
+
+        // Assert - validate response: verify Animal fields
+        assertThat(animalResponse.getName(), equalTo("Hela"));
+        assertThat(animalResponse.getBreed(), equalTo("Mestizo"));
+        assertThat(animalResponse.getGender(), equalTo("Female"));
+        assertThat(animalResponse.isVaccinated(), equalTo(false));
+        assertThat(animalResponse.getId(), notNullValue());
+
+        // Assert - Verify db creation.
+    }
+}
