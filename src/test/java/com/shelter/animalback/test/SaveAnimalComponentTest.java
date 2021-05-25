0c986c4be5183367bbea707923fd09026a914f32
Sebastian Villegas Zapata <svillegasz@psl.com.co>
2021-05-24 19:07:06 -0500

    Session 4: RestassuredMockMvc setup, using animal repository to assert db

--- src/test/java/com/shelter/animalback/test/SaveAnimalComponentTest.java
+++ src/test/java/com/shelter/animalback/test/SaveAnimalComponentTest.java
@@ -2,19 +2,38 @@ package com.shelter.animalback.test;
 
 import com.shelter.animalback.controller.dto.AnimalDto;
 import com.shelter.animalback.controller.dto.CreateAnimalBodyDto;
+import com.shelter.animalback.model.AnimalDao;
+import com.shelter.animalback.repository.AnimalRepository;
 import io.restassured.RestAssured;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.response.MockMvcResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SaveAnimalComponentTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private AnimalRepository animalRepository;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+    }
+
     @Test
     public void testSaveAnimalSuccessfully() {
         // Arrange - Instance animal with data for request body
@@ -25,11 +44,11 @@ public class SaveAnimalComponentTest {
         animalRequest.setVaccinated(false);
 
         // Act - Make a POST request against the create animal endpoint
-        Response response = RestAssured.given()
+        MockMvcResponse response = RestAssuredMockMvc.given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(animalRequest)
                 .when()
-                .post("http://localhost:8080/animals")
+                .post("/animals")
                 .thenReturn();
 
         AnimalDto animalResponse = response.as(AnimalDto.class);
@@ -42,5 +61,13 @@ public class SaveAnimalComponentTest {
         assertThat(animalResponse.getId(), notNullValue());
 
         // Assert - Verify db creation.
+        Optional<AnimalDao> animalQuery = animalRepository.findById(animalResponse.getId());
+        assertThat(animalQuery.isPresent(), is(true));
+
+        AnimalDao animalEntity = animalQuery.get();
+        assertThat(animalEntity.getName(), equalTo("Hela"));
+        assertThat(animalEntity.getBreed(), equalTo("Mestizo"));
+        assertThat(animalEntity.getGender(), equalTo("Female"));
+        assertThat(animalEntity.isVaccinated(), equalTo(false));
     }
 }
