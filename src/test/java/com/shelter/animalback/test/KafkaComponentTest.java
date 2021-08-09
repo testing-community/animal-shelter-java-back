327d2263401a5ef5094a4aa676789fa913d0816e
Sebastian Villegas Zapata <sebastian.villegasz@perficient.com>
2021-08-09 17:23:14 -0500

    added: pet listener component test

--- src/test/java/com/shelter/animalback/test/KafkaComponentTest.java
+++ src/test/java/com/shelter/animalback/test/KafkaComponentTest.java
@@ -1,10 +1,15 @@
 package com.shelter.animalback.test;
 
+import com.shelter.animalback.controller.dto.AnimalDto;
 import com.shelter.animalback.controller.dto.CreateAnimalBodyDto;
+import com.shelter.animalback.integration.PetsListener;
+import com.shelter.animalback.model.AnimalDao;
+import com.shelter.animalback.repository.AnimalRepository;
 import com.shelter.animalback.test.config.TestApplication;
 import com.shelter.animalback.test.config.TestContextInitializer;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import io.restassured.module.mockmvc.response.MockMvcResponse;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +20,14 @@ import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.hamcrest.Matchers.is;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TestApplication.class)
@@ -27,6 +38,12 @@ public class KafkaComponentTest {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    private PetsListener petsListener;
+
+    @Autowired
+    private AnimalRepository animalRepository;
 
     @BeforeEach
     public void setUp() {
@@ -74,5 +91,27 @@ public class KafkaComponentTest {
                 .then()
                 .status(HttpStatus.BAD_REQUEST)
                 .body(is("Interrupted"));
+    }
+
+    @Test
+    public void testSaveAnimalSuccessfully() throws JsonProcessingException {
+        // Arrange - Instance animal with data for request body
+        ObjectMapper objectMapper = new ObjectMapper();
+        CreateAnimalBodyDto animalRequest = new CreateAnimalBodyDto();
+        animalRequest.setName("Firulais");
+        animalRequest.setBreed("Mestizo");
+        animalRequest.setGender("Male");
+        animalRequest.setVaccinated(false);
+
+        // Act - Make a POST request against the create animal endpoint
+        petsListener.receive(objectMapper.writeValueAsString(animalRequest));
+
+        // Assert - Verify db creation.
+        AnimalDao animal = animalRepository.findByName("Firulais");
+
+        assertThat(animal.getName(), equalTo("Firulais"));
+        assertThat(animal.getBreed(), equalTo("Mestizo"));
+        assertThat(animal.getGender(), equalTo("Male"));
+        assertThat(animal.isVaccinated(), equalTo(false));
     }
 }
